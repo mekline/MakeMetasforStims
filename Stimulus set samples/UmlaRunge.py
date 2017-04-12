ca80fa63a53dfa8137cde9798b17b1357c0d3d46
clairekpinson <clairepinson@college.harvard.edu>
2017-04-12 15:56:11 -0400

    Umla Runge Conditionlist

--- Stimulus set samples/UmlaRunge.py
+++ Stimulus set samples/UmlaRunge.py
@@ -5,6 +5,7 @@ movie_items = []
 
 def convert_to_yaml(line,counter):
 	itemNo = int(line[0])
+	#make groupNo a condition
 	groupNo = int(line[1])
 	yamlfilename = line[2].split('.')[0] + '.yaml'
 	movieFilename = line[2]
@@ -15,6 +16,7 @@ def convert_to_yaml(line,counter):
 	Source = line[7]
 	Goal = line[8]
 	sentenceDescription = [line[9], line[10], line[11], line[12], line[13]]
+	#versionletter and versionlabel are conditions
 	versionletter = line[14]
 	versionlabel = line[15]
 	
@@ -22,17 +24,26 @@ def convert_to_yaml(line,counter):
 
 	movie_item = {
 		'ItemNo': itemNo,
-		'GroupNo': groupNo,
+		#'GroupNo': groupNo,
 		'movieFilename': movieFilename,
 		'SentenceDescription': sentenceDescription,
-		'yamlFilename': yamlfilename,
-		'VersionLabel': versionlabel
+		'yamlFilename': yamlfilename#,
+		#'VersionLabel': versionlabel
 	}
 
-#how do I create a loop for just the versionletter if it weren't in a list?
-	versionletterAB = versionletter
-	if versionletterAB != '':
-		movie_item.update({'VersionLetter': versionletterAB})
+	conditionlist = []
+	possibleconditions = [versionletter, versionlabel, groupNo]
+	conditionlabel = ['Version', 'Movie Set', 'Group Number']
+	for i in xrange(0, len(possibleconditions)):
+		if possibleconditions[i] != '':
+			newcondition = {conditionlabel[i]: possibleconditions[i]}
+			conditionlist.append(newcondition)
+
+	movie_item.update({'conditionlist': conditionlist})
+
+	#versionletterAB = versionletter
+	#if versionletterAB != '':
+	#	movie_item.update({'VersionLetter': versionletterAB})
 
 	participantlist = []
 	possibleroles = [Agent, Patient, Instrument, Theme, Source, Goal]
